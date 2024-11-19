4286e381263f76647df650eef4cd7e612bf4daf9
Leo-oxx <86710275+Leo-oxx@users.noreply.github.com>
2024-11-19 16:42:05 +0800

    Update Vectors in more detail.v

--- Verilog Language/Vectors/Vectors in more detail.v
+++ Verilog Language/Vectors/Vectors in more detail.v
@@ -1,7 +1,11 @@
 //构建一个组合电路，将输入的半字（16 位，[15:0] ）分成低 [7:0] 字节和高 [15:8] 字节。
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
 module top_module( 
     input wire [15:0] in,
     output wire [7:0] out_hi,
     output wire [7:0] out_lo );
 
+    assign out_hi = in[15:8];
+    assign out_lo = in[7:0];
+    
 endmodule
